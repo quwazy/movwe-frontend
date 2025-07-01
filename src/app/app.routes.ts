dfcab12ffd1d6fa06c713da2126e7980913bf41b
quwazy <jankoristic@proton.me>
2025-07-01 13:50:27 +0200

    navbar and client preview

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,24 @@ import { Routes } from '@angular/router';
 
 import { LoginView } from './employee/login-view/login-view';
 import { ClientView } from './employee/client-view/client-view';
+import { EmployeeView } from './employee/employee-view/employee-view';
+import { MovieView } from './employee/movie-view/movie-view';
 
 export const routes: Routes = [
   { 
-    path: 'login',
+    path: '',
     component: LoginView 
   },
   {
     path: 'client-view',
     component: ClientView
+  },
+  {
+    path: 'employee-view',
+    component: EmployeeView
+  },
+  {
+    path: 'movie-view',
+    component: MovieView
   }
 ];
--- src/app/employee/client-view/client-view.ts
+++ src/app/employee/client-view/client-view.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Client } from '../models/client.interface';
 import { ClientService } from '../services/client-service';
+import { NavBar } from '../nav-bar/nav-bar';
 
 @Component({
   selector: 'app-client-view',
-  imports: [CommonModule],
+  imports: [CommonModule, NavBar, FormsModule],
   templateUrl: './client-view.html',
   styleUrl: './client-view.css'
 })
 export class ClientView implements OnInit {
+  protected selectedClient: Client | null = null;
   protected clientList: Array<Client> = [];
+
+  searchId: number | null = null;
+  searchEmail: string | null = null;
 
   constructor(private clientService: ClientService) { }
   
@@ -27,5 +33,41 @@ export class ClientView implements OnInit {
         alert('Error loading clients. ' + error.message);
       }
     });
+  }
+
+  private searchClientById(id: number) {
+    this.clientService.getClientById(id).subscribe({
+      next: (client) => {
+        this.selectedClient = client;
+      },
+      error: (error) => {
+        alert('Error loading client. ' + error.message);
+      }
+    });
+  }
+
+  private searchClientByEmail(email: string) {
+    this.clientService.getClientByEmail(email).subscribe({
+      next: (client) => {
+        this.selectedClient = client;
+      },
+      error: (error) => {
+        alert('Error loading client. ' + error.message);
+      }
+    });
+  }
+
+  onSearchById(): void {
+    if(this.searchId != null){
+      this.searchClientById(this.searchId);
+      this.searchId = null;
+    }
+  }
+
+  onSearchByEmail(): void {
+    if(this.searchEmail != null){
+      this.searchClientByEmail(this.searchEmail);
+      this.searchEmail = "";
+    }
   }
 }
