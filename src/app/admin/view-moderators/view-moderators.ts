130787c0bb6b264c0a03f6b5402a15a3a0169bd2
quwazy <jankoristic@proton.me>
2025-07-17 11:35:49 +0200

    more initial admin components

---
+++ src/app/admin/view-moderators/view-moderators.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-moderators',
+  imports: [],
+  templateUrl: './view-moderators.html',
+  styleUrl: './view-moderators.css'
+})
+export class ViewModerators {
+
+}
---
+++ src/app/admin/view-movies/view-movies.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-movies',
+  imports: [],
+  templateUrl: './view-movies.html',
+  styleUrl: './view-movies.css'
+})
+export class ViewMovies {
+
+}
